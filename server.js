f93b028672bf1904838f90a1c045a71edba6c3fb
Hamza Hayd <dado.org@hotmail.com>
2021-08-22 11:54:20 +0200

    changed the port number

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*const jsonServer = require('json-server')
+const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -10,18 +10,5 @@ server.use(router)
 server.listen(PORT, () => {
  console.log(`Server is running on port ${PORT} `)
 })
-*/
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middleware = jsonServer.defaults({ noCors: true })
 
-const { PORT = 3000 } = process.env
-
-server.use(middleware)
-server.use(router)
-
-server.listen(PORT, () => {
- console.log(`JSON Server running on port ${PORT}`)
-})
